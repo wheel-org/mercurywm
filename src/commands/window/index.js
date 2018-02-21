022d6cb2ee68c02fe9a54567144183a0516ab416
Felix Guo <felixguoxiuping@gmail.com>
2018-02-21 15:02:55 -0800

    Added window list command

--- src/commands/window/index.js
+++ src/commands/window/index.js
@@ -14,6 +14,21 @@ function run(state, params) {
         case 'top':
         case 'bottom':
             return this.execute(shift);
+        case 'list':
+            this.output('In this workspace:');
+            for (var i = 0; i < this.workspace.windows.length; i++) {
+                var outputString = "[" + i + "] ";
+                if (this.workspace.windows[i].terminal.inProg) {
+                    outputString += "Running: ";
+                    outputString += this.workspace.windows[i].terminal.runningCommand + " ";
+                    outputString += this.workspace.windows[i].terminal.params.join(" ");
+                }
+                else {
+                    outputString += "Idle"
+                }
+                this.output(outputString)
+            }
+            return state;
         default:
             this.output('Unknown parameter ' + params[0]);
             return state;
