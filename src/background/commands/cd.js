2d005dee28d6fbef67076931aba6e8d47026f350
Michael Kim <micklepickle98@gmail.com>
2018-04-02 22:23:04 -0700

    Fix cd

--- src/background/commands/cd.js
+++ src/background/commands/cd.js
@@ -8,9 +8,17 @@ export default function cd(state: StoreState, params: Array<string>) {
   if (params.length === 0) {
     this.terminal.workingDirectory = '~';
   } else if (params.length === 1) {
-    const dir = getDirectory(params[0], this.terminal.workingDirectory);
+    const dir = getDirectory(
+      params[0],
+      state.wfs,
+      this.terminal.workingDirectory
+    );
     if (dir) {
-      const path = getPath(params[0], this.terminal.workingDirectory);
+      const path = getPath(
+        params[0],
+        state.wfs,
+        this.terminal.workingDirectory
+      );
       this.terminal.workingDirectory = path;
     } else {
       this.output('No such directory ' + params[0]);
