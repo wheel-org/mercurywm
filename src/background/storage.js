7a676ef9471a4c4f49b12a8125a16f2f711d194a
Felix Guo <felixguoxiuping@gmail.com>
2018-05-01 12:16:01 -0700

    Set version number based on package.json and use default title instead of setting the environment variable

--- src/background/storage.js
+++ src/background/storage.js
@@ -1,66 +1,59 @@
 /* @flow */
 
 import setupFile from 'background/setup';
-import Constants, {
-  createDirectory,
-  createFile,
-  createWorkspace
-} from 'constants.js';
+import Constants, { createDirectory, createFile, createWorkspace } from 'constants.js';
 
 import type { StorageState, StoreState } from 'types';
 
 // INITIAL STATE
 const id = Date.now();
 const defaultStateObject: StorageState = {
-  [Constants.STATE_KEY]: {
-    loaded: false,
-    workspaces: [createWorkspace(id)],
-    wfs: createDirectory('~', [
-      createFile(
-        'Welcome',
-        'Welcome to MercuryWM! Type `setup` to get started!\n'
-      ),
-      createDirectory('.bin', [createFile('setup', setupFile)])
-    ]),
-    wsh: {
-      // environmental variables
-      env: {
-        background: '#555',
-        title: Constants.NAME + ' ' + Constants.VERSION,
-        prompt: '%w $ ',
-        username: Constants.NAME
-      }
-    },
-    selectedWindow: id,
-    selectedWorkspace: 0
-  }
+    [Constants.STATE_KEY]: {
+        loaded: false,
+        workspaces: [createWorkspace(id)],
+        wfs: createDirectory('~', [
+            createFile('Welcome', 'Welcome to MercuryWM! Type `setup` to get started!\n'),
+            createDirectory('.bin', [createFile('setup', setupFile)])
+        ]),
+        wsh: {
+            // environmental variables
+            env: {
+                background: '#555',
+                title: '',
+                prompt: '%w $ ',
+                username: Constants.NAME
+            }
+        },
+        selectedWindow: id,
+        selectedWorkspace: 0
+    }
 };
 
 function load(callback: StoreState => void) {
-  chrome.storage.local.get(defaultStateObject, (data: StorageState) => {
-    callback(data[Constants.STATE_KEY]);
-  });
+    chrome.storage.local.get(defaultStateObject, (data: StorageState) => {
+        callback(data[Constants.STATE_KEY]);
+    });
 }
 
 function clear() {
-  // Need to deep copy default state
-  const newState = {
-    ...JSON.parse(JSON.stringify(defaultStateObject[Constants.STATE_KEY])),
-    loaded: true
-  };
-  chrome.storage.local.set({ [Constants.STATE_KEY]: newState });
-  return newState;
+    // Need to deep copy default state
+    const newState = {
+        ...JSON.parse(JSON.stringify(defaultStateObject[Constants.STATE_KEY])),
+        loaded: true
+    };
+    chrome.storage.local.set({ [Constants.STATE_KEY]: newState });
+    return newState;
 }
 
 function save(state: StoreState) {
-  if (state.loaded) {
-    chrome.storage.local.set({ [Constants.STATE_KEY]: state });
-  }
+    if (state.loaded) {
+        chrome.storage.local.set({ [Constants.STATE_KEY]: state });
+    }
 }
 
 module.exports = {
-  clear,
-  load,
-  save,
-  initialState: defaultStateObject[Constants.STATE_KEY]
+    clear,
+    load,
+    save,
+    initialState: defaultStateObject[Constants.STATE_KEY]
 };
