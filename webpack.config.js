4b9af153a236d161f71df58048bda206981268a5
Felix Guo <fg123@users.noreply.github.com>
2018-04-30 23:49:07 -0700

    Replaced instances of MERCURYWM_URL with constant source (#4)

--- webpack.config.js
+++ webpack.config.js
@@ -2,79 +2,72 @@ const path = require('path');
 const webpack = require('webpack');
 
 const config = {
-  entry: {
-    main: './src/mercury/index.jsx',
-    background: './src/background/index.js',
-    render: './src/render/index.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: [
-            [
-              'env',
-              {
-                targets: {
-                  browser: 'last 2 versions'
+    entry: {
+        main: './src/mercury/index.jsx',
+        background: './src/background/index.js',
+        render: './src/render/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: [
+                        [
+                            'env',
+                            {
+                                targets: {
+                                    browser: 'last 2 versions'
+                                }
+                            }
+                        ],
+                        'react',
+                        'stage-2'
+                    ]
                 }
-              }
-            ],
-            'react',
-            'stage-2'
-          ]
-        }
-      },
-      {
-        test: /\.jsx?$/,
-        enforce: 'pre',
-        loader: 'remove-flow-types-loader',
-        include: path.join(__dirname, 'src')
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.MERCURYWM_URL': JSON.stringify(
-        'https://wheel-org.github.io/mercurywm-scripts/'
-      )
-    })
-  ],
-  stats: {
-    colors: true
-  },
-  resolve: {
-    modules: [path.resolve('./node_modules'), path.resolve('./src')]
-  }
+            },
+            {
+                test: /\.jsx?$/,
+                enforce: 'pre',
+                loader: 'remove-flow-types-loader',
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    resolve: {
+        modules: [path.resolve('./node_modules'), path.resolve('./src')]
+    }
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-      compress: {
-        warnings: false,
-        drop_console: true
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  );
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            comments: false,
+            compress: {
+                warnings: false,
+                drop_console: true
+            }
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    );
 } else {
-  config.devtool = '#cheap-module-source-map';
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env.UPDEEP_MODE': JSON.stringify('dangerously_never_freeze')
-    })
-  );
+    config.devtool = '#cheap-module-source-map';
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env.UPDEEP_MODE': JSON.stringify('dangerously_never_freeze')
+        })
+    );
 }
 
 module.exports = config;
