d0b3c807dda724462eb36d23f472e90985a61237
Michael Kim <micklepickle98@gmail.com>
2019-05-14 10:31:35 -0700

    Fix issue with latest version updater

--- src/background/reducers/updater.js
+++ src/background/reducers/updater.js
@@ -1,35 +1,48 @@
 /* @flow strict */
 
+import u from 'updeep';
+
 import type { StoreState } from 'types';
 
+// Updates should be safe to perform even for updated users
 const updates = {
-  '2.0.0': state => ({
-    ...state,
-    aliases: {}
-  })
+  '2.0.0': (state: StoreState) =>
+    u(
+      {
+        wsh: {
+          aliases: {}
+        }
+      },
+      state
+    )
 };
 
 // List of all version strings
-const versions = ['1.0.0', '1.1.0', '2.0.0', '2.1.0'];
+// The last version in the list must be the current version in package.json
+const versions = ['1.0.0', '1.1.0', '2.0.0'];
 
-function updateState(state: $Shape<StoreState>): StoreState {
-  // While the state isn't updated to the latest version, keep applying updates
-  while (state.version !== Constants.VERSION) {
-    const index = versions.findIndex(v => v === state.version);
-    if (index === -1) {
-      // Invalid version string; try all updates
+function updateState(state: StoreState): StoreState {
+  let index = versions.findIndex(v => v === state.version);
+  // Invalid version string; try all updates
+  if (index === -1) {
+    // $FlowFixMe: mutating state
+    state.version = '1.0.0';
+  }
+
+  // Keep applying updaters until the state is at the latest version
+  while (index < versions.length) {
+    const update = updates[state.version];
+    if (update) {
       // $FlowFixMe: mutating state
-      state.version = '1.0.0';
-    } else {
-      const update = updates[state.version];
-      if (update) {
-        // $FlowFixMe: mutating state
-        state = update(state);
-      }
+      state = update(state);
+    }
 
+    if (state.version !== Constants.VERSION) {
       // $FlowFixMe: mutating state
       state.version = versions[index + 1];
     }
+
+    index++;
   }
 
   return state;
